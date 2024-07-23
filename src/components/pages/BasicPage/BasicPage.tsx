bc47a00b0025ec65f5659bb43ece2685fce352b3
alexeiVinnichek <alexei.vinnichek@mail.ru>
2024-07-23 11:00:00 +0300

    some styles chsnged

--- src/components/pages/BasicPage/BasicPage.tsx
+++ src/components/pages/BasicPage/BasicPage.tsx
@@ -1,17 +1,19 @@
 import { Outlet } from "react-router-dom";
 
 const BasicPage = () => {
-  return (
-    <div className="basic-page">
-        <header>
-            <h2>Header</h2>
-        </header>
-        <Outlet />
-        <footer>
-            <h2>Footer</h2>
-        </footer>
-    </div>
-  )
+    return (
+        <div className="basic-page">
+            <header>
+                <h2>Quiz Application</h2>
+            </header>
+            <div className="body">
+                <Outlet />
+            </div>
+            <footer>
+                <h2>Andersen react-intensive 2024</h2>
+            </footer>
+        </div>
+    );
 }
 
 export { BasicPage };

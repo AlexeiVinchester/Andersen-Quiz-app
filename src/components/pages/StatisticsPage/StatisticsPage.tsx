8689415f8d309a9e089c63055c0996675f539dfc
alexeiVinnichek <alexei.vinnichek@mail.ru>
2024-07-31 22:30:40 +0300

    created new component for statistics data and added them to Statistics Page

--- src/components/pages/StatisticsPage/StatisticsPage.tsx
+++ src/components/pages/StatisticsPage/StatisticsPage.tsx
@@ -1,11 +1,12 @@
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { Store } from "../../../redux/store/interface/store.interface";
-import { ResultContainer } from "../ResultQuizPage/ResultContainer/ResultContainer";
+import { InfoContainer } from "../../spreadedComponents/InfoContainer/InfoContainer";
 import { ResultNumberField } from "../ResultQuizPage/ResultNumberField/ResultNumberField";
 import { StyledButton } from "../../spreadedComponents/StyledButton/StyledButton";
 import { START } from "../../Router/routes";
 import { Statistics } from "../../../redux/slices/interfaces/statisticsSlice.interface";
+import { StatisticsContainer } from "./StatisticsContainer/StatisticsContainer";
 
 const StatisticsPage = () => {
     const { totalQuestions, totalCorrectAnswers, difficulties, categories, types }: Statistics = useSelector((state: Store) => state.statistics);
@@ -15,48 +16,21 @@ const StatisticsPage = () => {
         navigate(START);
     };
 
-    console.log(Object.entries(difficulties))
-
     return (
         <div className="statistics-page page-container flex-col">
             <h4>Full statistics of questions for full period</h4>
             <ResultNumberField text="Questions" value={totalQuestions} />
             <ResultNumberField text="Correct Answers" value={totalCorrectAnswers} />
-
             <div className="flex-row statistics-group">
-                <ResultContainer header="Difficulties">
-                    {
-                        Object.entries(difficulties).map(item => (
-                            <ResultNumberField
-                                key={item[0]}
-                                text={item[0]}
-                                value={item[1]}
-                            />
-                        ))
-                    }
-                </ResultContainer>
-                <ResultContainer header="Categories">
-                    {
-                        Object.entries(categories).map(item => (
-                            <ResultNumberField
-                                key={item[0]}
-                                text={item[0]}
-                                value={item[1]}
-                            />
-                        ))
-                    }
-                </ResultContainer>
-                <ResultContainer header="Types">
-                    {
-                        Object.entries(types).map(item => (
-                            <ResultNumberField
-                                key={item[0]}
-                                text={item[0]}
-                                value={item[1]}
-                            />
-                        ))
-                    }
-                </ResultContainer>
+                <InfoContainer header="Difficulties">
+                    <StatisticsContainer data={difficulties} />
+                </InfoContainer>
+                <InfoContainer header="Categories">
+                    <StatisticsContainer data={categories} />
+                </InfoContainer>
+                <InfoContainer header="Types">
+                    <StatisticsContainer data={types} /> 
+                </InfoContainer>
             </div>
             <div className="statistics-button-to-start">
                 <StyledButton
--- src/components/spreadedComponents/InfoContainer/interface/infoContainer.interface.ts
+++ src/components/spreadedComponents/InfoContainer/interface/infoContainer.interface.ts
@@ -1,6 +1,6 @@
 import React from "react";
 
-export interface ResultContainerProps {
+export interface InfoContainerProps {
     header: string,
     children: React.ReactNode
 }

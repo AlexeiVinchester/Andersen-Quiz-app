864640a6661d4bc92c1924bb6598da5501f8a06e
alexeiVinnichek <alexei.vinnichek@mail.ru>
2024-07-31 21:28:10 +0300

    1. added new fields for Store interface 2. added all data to statistics page

--- src/redux/store/interface/store.interface.ts
+++ src/redux/store/interface/store.interface.ts
@@ -1,8 +1,12 @@
 import { QuizConfig } from "../../../components/spreadedInterfaces/quizConfig.interface";
 import { Result } from "../../../components/spreadedInterfaces/result.interface";
 import { Question } from "../../../components/pages/MainQuizPage/QuestionContainer/interface/question.interface";
+import { Data } from "../../slices/interfaces/loadQuestionsSlice.interface";
+import { Statistics } from "../../slices/interfaces/statisticsSlice.interface";
 export interface Store {
     configuration: QuizConfig,
     result: Result,
-    questions: Question[]
+    questions: Question[],
+    loadedQuestions: Data[],
+    statistics: Statistics
 }

77acf38beaa32aaa96ce7693d9d5f85f7d463032
alexeiVinnichek <alexei.vinnichek@mail.ru>
2024-08-01 10:49:17 +0300

    fixed comment

--- src/components/pages/StatisticsPage/StatisticsContainer/StatisticsContainer.tsx
+++ src/components/pages/StatisticsPage/StatisticsContainer/StatisticsContainer.tsx
@@ -2,19 +2,13 @@ import { ResultNumberField } from "../../ResultQuizPage/ResultNumberField/Result
 import { StatisticsContainerProps } from "./interface/StatisticsContainer.interface";
 
 const StatisticsContainer = ({ data }: StatisticsContainerProps) => {
-    return (
-        <>
-            {
-                Object.entries(data).map(item => (
+    return Object.entries(data).map(item => (
                     <ResultNumberField
                         key={item[0]}
                         text={item[0]}
                         value={item[1]}
                     />
                 ))
-            }
-        </>
-    );
-}
+};
 
 export { StatisticsContainer };

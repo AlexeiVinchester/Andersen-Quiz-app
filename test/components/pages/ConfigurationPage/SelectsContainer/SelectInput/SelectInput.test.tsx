eb632f803d010670f01125370b4868c545dd0ab0
alexeiVinnichek <alexei.vinnichek@mail.ru>
2024-08-22 16:03:26 +0300

    improved tests for selects of quiz configuration

--- test/components/pages/ConfigurationPage/SelectsContainer/SelectInput/SelectInput.test.tsx
+++ test/components/pages/ConfigurationPage/SelectsContainer/SelectInput/SelectInput.test.tsx
@@ -1,42 +1,84 @@
-import { describe, test, vi, beforeEach, expect } from "vitest";
+import { describe, test, vi, expect, Mock, afterEach } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { SelectInput } from '../../../../../../src/components/pages/ConfigurationPage/SelectsContainer/SelectInput/SelectInput';
 import React from "react";
 import { userEvent } from '@testing-library/user-event'
-import { categories, types, difficulties, times } from '../../../../../../src/components/pages/ConfigurationPage/SelectsContainer/options/options';
+import { categories, difficulties, types, times } from '../../../../../../src/components/pages/ConfigurationPage/SelectsContainer/options/options';
+import { Option } from "../../../../../../src/components/pages/ConfigurationPage/SelectsContainer/options/option.interface";
+
+const customRender = (onChangeSelectField: Mock, data: Option[], label: string) => {
+    const selectTestId = 'test-id-for-SelectInput';
+    render(
+        <SelectInput
+            data-testid={selectTestId}
+            options={data}
+            label={label}
+            onChangeSelectField={onChangeSelectField}
+        />
+    );
+};
 
 describe('testing of SelectInputs of quiz configuration', () => {
 
     describe('testing of select for categories', () => {
         const selectTestId = 'test-id-for-SelectInput';
         const onChangeSelectField = vi.fn();
-        let select: HTMLSelectElement;
-        beforeEach(() => {
-            render(
-                <SelectInput
-                    data-testid={selectTestId}
-                    options={categories}
-                    label="category"
-                    onChangeSelectField={onChangeSelectField}
-                />);
-            select = screen.getByTestId(selectTestId)
+
+        afterEach(() => {
+            vi.resetAllMocks();
         });
 
         test('SelectInput uses html tag select', () => {
-            expect(select.tagName).toBe('SELECT')
+            customRender(onChangeSelectField, categories, 'category');
+            expect(screen.getByTestId(selectTestId).tagName).toBe('SELECT')
         });
 
         test(`SelectInput for categories has ${categories.length} options`, () => {
+            customRender(onChangeSelectField, categories, 'category');
             expect(screen.getAllByRole('option').length).toBe(categories.length)
         });
 
         test('Check of default selected category', () => {
+            customRender(onChangeSelectField, categories, 'category');
             expect(screen.getByRole('option', { name: 'Sports', selected: true }));
         });
 
         test('User change selected category', async () => {
             categories.forEach(async (category) => {
-                await userEvent.selectOptions(select, category.label);
+                customRender(onChangeSelectField, categories, 'category');
+                await userEvent.selectOptions(screen.getAllByTestId(selectTestId)[0], category.label);
+                expect(onChangeSelectField).toBeCalled();
+            });
+        });
+    });
+
+    describe('testing of select for difficulties', () => {
+        const selectTestId = 'test-id-for-SelectInput';
+        const onChangeSelectField = vi.fn();
+
+        afterEach(() => {
+            vi.resetAllMocks();
+        });
+
+        test('SelectInput uses html tag select', () => {
+            customRender(onChangeSelectField, difficulties, 'difficulty');
+            expect(screen.getByTestId(selectTestId).tagName).toBe('SELECT')
+        });
+
+        test(`SelectInput for difficulties has ${difficulties.length} options`, () => {
+            customRender(onChangeSelectField, difficulties, 'difficulty');
+            expect(screen.getAllByRole('option').length).toBe(difficulties.length)
+        });
+
+        test('Check of default selected difficulty', () => {
+            customRender(onChangeSelectField, difficulties, 'difficulty');
+            expect(screen.getByRole('option', { name: 'Easy', selected: true }));
+        });
+
+        test('User change selected difficulty', async () => {
+            difficulties.forEach(async (difficulty) => {
+                customRender(onChangeSelectField, difficulties, 'difficulty');
+                await userEvent.selectOptions(screen.getAllByTestId(selectTestId)[0], difficulty.label);
                 expect(onChangeSelectField).toBeCalled();
             });
         });
@@ -45,68 +87,30 @@ describe('testing of SelectInputs of quiz configuration', () => {
     describe('testing of select for types', () => {
         const selectTestId = 'test-id-for-SelectInput';
         const onChangeSelectField = vi.fn();
-        let select: HTMLSelectElement;
-        beforeEach(() => {
-            render(
-                <SelectInput
-                    data-testid={selectTestId}
-                    options={types}
-                    label="type"
-                    onChangeSelectField={onChangeSelectField}
-                />);
-            select = screen.getByTestId(selectTestId)
+
+        afterEach(() => {
+            vi.resetAllMocks();
         });
 
         test('SelectInput uses html tag select', () => {
-            expect(select.tagName).toBe('SELECT')
+            customRender(onChangeSelectField, types, 'type');
+            expect(screen.getByTestId(selectTestId).tagName).toBe('SELECT')
         });
 
-        test(`SelectInput for types has ${types.length} options`, () => {
+        test(`SelectInput for types has ${difficulties.length} options`, () => {
+            customRender(onChangeSelectField, types, 'type');
             expect(screen.getAllByRole('option').length).toBe(types.length)
         });
 
         test('Check of default selected type', () => {
+            customRender(onChangeSelectField, types, 'type');
             expect(screen.getByRole('option', { name: 'Boolean', selected: true }));
         });
 
         test('User change selected type', async () => {
             types.forEach(async (type) => {
-                await userEvent.selectOptions(select, type.label);
-                expect(onChangeSelectField).toBeCalledTimes(1);
-            });
-        });
-    });
-
-    describe('testing of select for difficulties', () => {
-        const selectTestId = 'test-id-for-SelectInput';
-        const onChangeSelectField = vi.fn();
-        let select: HTMLSelectElement;
-        beforeEach(() => {
-            render(
-                <SelectInput
-                    data-testid={selectTestId}
-                    options={difficulties}
-                    label="difficulty"
-                    onChangeSelectField={onChangeSelectField}
-                />);
-            select = screen.getByTestId(selectTestId)
-        });
-
-        test('SelectInput uses html tag select', () => {
-            expect(select.tagName).toBe('SELECT')
-        });
-
-        test(`SelectInput for difficulties has ${difficulties.length} options`, () => {
-            expect(screen.getAllByRole('option').length).toBe(difficulties.length)
-        });
-
-        test('Check of default selected difficulty', () => {
-            expect(screen.getByRole('option', { name: 'Easy', selected: true }));
-        });
-
-        test('User change selected difficulty', async () => {
-            difficulties.forEach(async (difficulty) => {
-                await userEvent.selectOptions(select, difficulty.label);
+                customRender(onChangeSelectField, types, 'type');
+                await userEvent.selectOptions(screen.getAllByTestId(selectTestId)[0], type.label);
                 expect(onChangeSelectField).toBeCalled();
             });
         });
@@ -115,33 +119,30 @@ describe('testing of SelectInputs of quiz configuration', () => {
     describe('testing of select for times', () => {
         const selectTestId = 'test-id-for-SelectInput';
         const onChangeSelectField = vi.fn();
-        let select: HTMLSelectElement;
-        beforeEach(() => {
-            render(
-                <SelectInput
-                    data-testid={selectTestId}
-                    options={times}
-                    label="time"
-                    onChangeSelectField={onChangeSelectField}
-                />);
-            select = screen.getByTestId(selectTestId)
+
+        afterEach(() => {
+            vi.resetAllMocks();
         });
 
         test('SelectInput uses html tag select', () => {
-            expect(select.tagName).toBe('SELECT')
+            customRender(onChangeSelectField, times, 'time');
+            expect(screen.getByTestId(selectTestId).tagName).toBe('SELECT')
         });
 
         test(`SelectInput for times has ${times.length} options`, () => {
+            customRender(onChangeSelectField, times, 'time');
             expect(screen.getAllByRole('option').length).toBe(times.length)
         });
 
         test('Check of default selected time', () => {
+            customRender(onChangeSelectField, times, 'time');
             expect(screen.getByRole('option', { name: '1m', selected: true }));
         });
 
-        test('User change selected category', async () => {
+        test('User change selected time', async () => {
             times.forEach(async (time) => {
-                await userEvent.selectOptions(select, time.label);
+                customRender(onChangeSelectField, times, 'time');
+                await userEvent.selectOptions(screen.getAllByTestId(selectTestId)[0], time.label);
                 expect(onChangeSelectField).toBeCalled();
             });
         });

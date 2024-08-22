d1b2c76d9e95d218c492d4604586249d6652b383
alexeiVinnichek <alexei.vinnichek@mail.ru>
2024-08-22 15:32:18 +0300

    improved test for input of number of questions

--- test/components/pages/ConfigurationPage/NumberInputContainer/NumberInputContainer.test.tsx
+++ test/components/pages/ConfigurationPage/NumberInputContainer/NumberInputContainer.test.tsx
@@ -1,42 +1,51 @@
-import { describe, test, vi, beforeEach, expect } from "vitest";
+import { describe, test, vi, expect, afterEach, Mock } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { NumberOfQuestionsContainer } from '../../../../../src/components/pages/ConfigurationPage/NumberInputContainer/NumberInputContainer';
 import React from "react";
 import { userEvent } from '@testing-library/user-event'
 
+const customRender = (onChangeNumber: Mock) => {
+    const inputTestId = 'test-id-for-NumberOfQuestionsContainer';
+    const placeholder = 'placeholder';
+    const label = 'label'
+    const min = 5;
+    const max = 15;
+
+    render(
+        <NumberOfQuestionsContainer
+            data-testid={inputTestId}
+            placeholder={placeholder}
+            label={label}
+            min={min}
+            max={max}
+            onChangeNumber={onChangeNumber}
+        />
+    );
+};
+
 describe('testing of NumberOfQuestionsContainer', () => {
     describe('testing of base features', () => {
         const inputTestId = 'test-id-for-NumberOfQuestionsContainer';
-        const placeholder = 'placeholder';
-        const label = 'label'
-        const min = 5;
-        const max = 15;
         const onChangeNumber = vi.fn();
-        let input: HTMLInputElement;
 
-        beforeEach(() => {
-            render(
-                <NumberOfQuestionsContainer
-                    data-testid={inputTestId}
-                    placeholder={placeholder}
-                    label={label}
-                    min={min}
-                    max={max}
-                    onChangeNumber={onChangeNumber}
-                />);
-            input = screen.getByTestId(inputTestId)
+        afterEach(() => {
+            vi.resetAllMocks();
         });
 
         test('check default value of NumberOfQuestionsContainer', () => {
-            expect(input.valueAsNumber).toBe(5)
+            customRender(onChangeNumber);
+            const input: HTMLInputElement = screen.getByTestId(inputTestId);
+            expect(input.value).toBe('5')
         });
 
         test('NumberOfQuestionsContainer uses html tag input', () => {
-            expect(input.tagName).toBe('INPUT')
+            customRender(onChangeNumber);
+            expect(screen.getByTestId(inputTestId).tagName).toBe('INPUT')
         });
 
         test('User change number of questions - called onChangeNumber', async () => {
-            await userEvent.type(input, '5666')
+            customRender(onChangeNumber);
+            await userEvent.type(screen.getByTestId(inputTestId), '5664')
             expect(onChangeNumber).toBeCalledTimes(4)
         });
     });
